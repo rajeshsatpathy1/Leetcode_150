714a9154c729d563d0e529c10e7d6f6e151eb2bd
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-09-27 00:34:01 -0500

    Add Remove Element code

---
+++ Remove_Element_27.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int n = nums.length;
+        int k = 0;
+        int valCount = 0;
+
+        for(int i=0;i<n;i++){
+            if(nums[i] == val)
+                valCount++;
+            else{
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        return n-valCount;
+    }
+}
