a860f9cbfc453372e28e72822f7b10e05bac5789
rajeshsatpathy1 <rajesh.a.s.1997@gmail.com>
2023-10-02 22:53:09 -0500

    Add Simplify Path. Use var

---
+++ Simplify_Path.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public String simplifyPath(String path) {
+        var pathArray = path.split("/");
+        var stack = new Stack<>();
+        var out = new StringBuffer();
+
+        for(int i=0;i<pathArray.length;i++){
+            var pathI = pathArray[i];
+            if(!pathI.equals(""))
+            {
+                if(pathI.equals("..")){
+                    if(!stack.isEmpty())
+                        stack.pop();
+                }else if(!path.equals("") && !pathI.equals(".")){
+                    stack.push(pathI);
+                }
+            }
+        }
+
+        while(!stack.isEmpty()){
+            out.insert(0, "/" + stack.pop());
+        }
+
+        if(out.length() == 0){
+            return "/";
+        }
+        
+        return out.toString();
+    }
+}
